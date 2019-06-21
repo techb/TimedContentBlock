e747e08d44c77d363d6e30155b082114b2baab6a
K.B. Carte <kb@digitalrelativity.com>
2019-06-21 15:51:21 -0400

    full working build with functionality

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 wp.blocks.registerBlockType('kbcarte/timed-content', {
     title: 'Timed Content',
-    icon: 'smiley',
+    icon: 'clock',
     category: 'common',
     attributes: {
-        posts: {type: 'array'}
+        start_date: {
+            type: 'string'
+        },
+        end_date: {
+            type: 'string'
+        }
     },
     edit: function(props){
-        function updateContent(event){
-            props.setAttributes({company_tags: event.target.value});
+        function updateStart(info){
+            props.setAttributes({start_date: info});
+        }
+
+        function updateEnd(info){
+            props.setAttributes({end_date: info});
         }
 
         return wp.element.createElement(
@@ -16,11 +25,51 @@ wp.blocks.registerBlockType('kbcarte/timed-content', {
               class: "content-wrapper"
             },
             wp.element.createElement("h3", null, "Timed Content"),
+            wp.element.createElement(
+                "div",
+                {
+                    class: "date-time-wrapper"
+                },
+                wp.element.createElement(
+                    "div",
+                    {
+                        class: "start-date-contain"
+                    },
+                    wp.element.createElement("label", {for: "start-date"}, "Start Date"),
+                    wp.element.createElement(wp.components.DateTimePicker, {
+                        name: "start-date",
+                        id: "start-date",
+                        onChange: updateStart,
+                        currentDate: props.attributes.start_date,
+                        is12Hour: true
+                    }),
+                ),
+                wp.element.createElement(
+                    "div",
+                    {
+                        class: "end-date-contain"
+                    },
+                    wp.element.createElement("label", {for: "end-date"}, "End Date"),
+                    wp.element.createElement(wp.components.DateTimePicker, {
+                        name: "end-date",
+                        id: "end-date",
+                        onChange: updateEnd,
+                        currentDate: props.attributes.end_date,
+                        is12Hour: true
+                    }),
+                ),
+            ),
+            wp.element.createElement(wp.editor.InnerBlocks),
         );
     },
+
     save: function(props){
-        // return is in plugin.php
-        // companyCardRend($props)
-        return null;
+        return wp.element.createElement(
+            "div",
+            {
+              class: "content-wrapper"
+            },
+            wp.element.createElement(wp.editor.InnerBlocks.Content),
+        );
     }
 });
--- plugin.php
+++ plugin.php
@@ -16,31 +16,37 @@ function loadBlockFiles() {
     wp_enqueue_script(
         'block-handle-script',
         plugin_dir_url( __FILE__ ).'index.js',
-        array('wp-blocks', 'wp-i18n', 'wp-editor'),
+        array('wp-blocks', 'wp-i18n', 'wp-editor', 'wp-element'),
         true
+    );
+    wp_enqueue_style(
+        'block-handle-css',
+        plugin_dir_url( __FILE__ ).'styles.css'
     );
 }
 add_action('enqueue_block_editor_assets', 'loadBlockFiles');
 
 
 function loadBlockFilesFrontend() {
-    wp_enqueue_style(
-        'block-handle-css',
-        plugin_dir_url( __FILE__ ).'styles.css'
-    );
+    // wp_enqueue_style(
+    //     'block-handle-css',
+    //     plugin_dir_url( __FILE__ ).'styles.css'
+    // );
 }
 add_action('enqueue_block_assets', 'loadBlockFilesFrontend');
 
 
-function timedContenRender($props) {
-    // split and trim white space, works without doing it
-    // just doing it to be safe
-    // var_dump($props);
-
-    $html = "<h2>Testing</h2>";
-
+function timedContenRender($props, $content) {
+    $html = "";
+    $start = new DateTime( str_replace( "T", " ", $props['start_date'] ) );
+    $end = new DateTime( str_replace( "T", " ", $props['end_date'] ) );
+    $now = new DateTime('now');
+    if ( $now >= $start && $now <= $end ){
+        $html = $content;
+    }
     return $html;
 }
+
 register_block_type( 'kbcarte/timed-content', array(
   'render_callback' => 'timedContenRender',
 ) );
